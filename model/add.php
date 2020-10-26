3be5ab0bb7f769965e838995cf1755cde02c16d5
MartinezFalconi <sergiomafa09@gmail.com>
2020-10-26 20:17:30 +0100

    Backup

--- model/add.php
+++ model/add.php
@@ -1,4 +1,5 @@
 <?php
+require_once '../controller/sessionController.php';
 
     echo "<FORM METHOD = 'POST' ACTION= 'addGo.php'>";
     echo "Nombre: <INPUT TYPE = 'TEXT' NAME = 'nombre' VALUE = ''><br>";
@@ -8,3 +9,5 @@
     echo "Email: <INPUT TYPE = 'TEXT' NAME = 'email' VALUE = ''><br>";
     echo "Password: <INPUT TYPE = 'password' NAME = 'pass' VALUE = ''><br>";
     echo "<INPUT TYPE = 'SUBMIT' NAME = 'añadir' VALUE = 'Añadir'><br>";
+
+echo "<h4><a href='../view/zona.admin.php'>Volver</a></h4>";
--- model/calculos.php
+++ model/calculos.php
@@ -1,6 +1,10 @@
 <?php
 require_once './connection.php';
 
+require_once '../controller/sessionController.php';
+
+/*MEDIAS DE ASIGNATURA + MEDIA MAS ALTA*/
+
 $sql0 = "SELECT AVG(nota) FROM tblnotas WHERE nomAsignatura = 'Matemáticas'";
 $result0 = mysqli_query($conexion,$sql0);
 
@@ -26,3 +30,55 @@ echo "<br>";
 $TOPMEDIA = max($MEDfisica,$MEDmats,$MEDprogramacion);
 echo "La media mas grande es: ".$TOPMEDIA;
 
+/*Alumnos con la nota mas alta*/
+
+$sql3 = "SELECT nombre, nota, nomAsignatura
+FROM tblnotas
+INNER JOIN tblalumnos
+ON tblnotas.idAlumnoFK = tblalumnos.idAlumno
+WHERE nomAsignatura = 'Matemáticas'
+ORDER BY nota DESC LIMIT 1";
+$result3 = mysqli_query($conexion,$sql3);
+
+$sql4 = "SELECT nombre, nota, nomAsignatura
+FROM tblnotas
+INNER JOIN tblalumnos
+ON tblnotas.idAlumnoFK = tblalumnos.idAlumno
+WHERE nomAsignatura = 'Fisica'
+ORDER BY nota DESC LIMIT 1";
+$result4 = mysqli_query($conexion,$sql4);
+
+$sql5 = "SELECT nombre, nota, nomAsignatura
+FROM tblnotas
+INNER JOIN tblalumnos
+ON tblnotas.idAlumnoFK = tblalumnos.idAlumno
+WHERE nomAsignatura = 'Programación'
+ORDER BY nota DESC LIMIT 1";
+$result5 = mysqli_query($conexion,$sql5);
+
+    echo "<table>";
+        echo "<tr>";
+        echo "<th>Alumno</th>";
+        echo "<th>Nota</th>";
+        echo "<th>Materia</th>";
+        echo "</tr>";
+        foreach ($result3 as $result3) {
+            echo "<td>{$result3['nombre']}</td>";
+            echo "<td>{$result3['nota']}</td>";
+            echo "<td>{$result3['nomAsignatura']}</td>";
+        }
+        foreach ($result4 as $result4) {
+            echo "<tr>";
+            echo "<td>{$result4['nombre']}</td>";
+            echo "<td>{$result4['nota']}</td>";
+            echo "<td>{$result4['nomAsignatura']}</td>";
+        }
+        foreach ($result5 as $result5) {
+            echo "<tr>";
+            echo "<td>{$result5['nombre']}</td>";
+            echo "<td>{$result5['nota']}</td>";
+            echo "<td>{$result5['nomAsignatura']}</td>";
+        }
+    echo "</table>";
+
+    echo "<h4><a href='../view/zona.admin.php'>Volver</a></h4>";
